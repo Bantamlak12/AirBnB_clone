7d1e4a77fa30ea09a689274cd097f23e54b4627e
Bantamlak <neoluka315@gmail.com>
2023-02-18 07:39:20 +0000

    updated user

--- models/user.py
+++ models/user.py
@@ -4,15 +4,14 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Defines a class User that inherits from BaseModel
+    """ Defines a class User that inherits from BaseModel
 
-    Public class attributes:
+        Public class attributes:
                     email: string - empty string
                     password: string - empty string
                     first_name: string - empty string
                     last_name: string - empty string
     """
-
     email = ""
     password = ""
     first_name = ""
